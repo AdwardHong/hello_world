f404603f246e14029318203b8b1d69f86a45fc51
AdwardHong <thesueprh@gmail.com>
2018-12-26 22:45:53 +0900

    Initial commit

---
+++ hello_world/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  hello_world
+//
+//  Created by JunSeok Hong on 2018. 12. 26..
+//  Copyright © 2018 adward. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
